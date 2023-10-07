b8920300fa1ec94303d65dd4eb6499c0e3713552
davidkpognon <dskpognon20@students.desu.edu>
2023-10-07 16:04:13 -0400

    3rd commit changes

--- lab1.c
+++ lab1.c
@@ -3,78 +3,40 @@
 #include <string.h>
 
 #define MAX_LINE_LENGTH 1024
+//Creating readstring function that will read what is in the file
+char* readString(char* filename) {
 
-// Method that reads line from a file 
-char* readString(FILE* file) {
-    char* line = NULL;
-    char buffer[MAX_LINE_LENGTH];
-
-    if (fgets(buffer, sizeof(buffer), file) != NULL) {
-        // Takes out the new line at the end of the file
-        size_t len = strlen(buffer);
-        if (len > 0 && buffer[len - 1] == '\n') {
-            buffer[len - 1] = '\0';
-            len--;
-        }
-
-        // Mallac allotcates space for the line
-        line = (char*)malloc(len + 1);
-        if (line != NULL) {
-            strcpy(line, buffer);
-        }
+    FILE* file =  fopen(filename,"r");//Fopen will open the file in read mode 
+    if (file == NULL) {//If file is empty, then reutrn a error message
+        fprintf(stderr ,"Can't find");
     }
-
-    return line;
+    char* string = (char*) malloc(MAX_LINE_LENGTH *sizeof(char));//allocate space for the string
+    if (string != NULL) {
+        fgets(string,MAX_LINE_LENGTH,file);
+        fclose(file);//closes the file pointer
+} else {
+    free(string);
 }
 
-// Methods that explodes a string
-char** explodeString(const char* str, char delimiter, int* count) {
-    if (str == NULL || count == NULL) {
-        return NULL;
-    }
-    *count = 1;
-    const char* ptr = str;
-    while (*ptr != '\0') {
-        if (*ptr == delimiter) {
-            (*count)++;
-        }
-        ptr++;
-    }
+    return string;
 
-    // Mallac allocates memory into the array
-    char** substrings = (char**)malloc(sizeof(char*) * (*count));
-    if (substrings == NULL) {
-        return NULL;
-    }
-
-    int i = 0;
-    ptr = str;
-    const char* start = str;
-
-    // Goes through the string and parses it
-    while (*ptr != '\0') {
-        if (*ptr == delimiter || *ptr == '\0') {
-            // Makes space for the string length
-            size_t length = ptr - start;
-            substrings[i] = (char*)malloc(length + 1);
-            if (substrings[i] == NULL) {
-                // Will return null if the space is not allocated
-                for (int j = 0; j < i; j++) {
-                    free(substrings[j]);
-                }
-                free(substrings);
-                return NULL;
-            }
-
-            strncpy(substrings[i], start, length);
-            substrings[i][length] = '\0';
-            i++;
-
-            // Iteration of the starting position of the stringS
-            start = ptr + 1;
-        }
-        ptr++;
-    }
-
-    return substrings;
 }
+
+
+char* mysteryExplode(const char* str) {//mystery explode takes the parameter of the type string from read string function
+    int len = strlen(str);
+    int explodedLength = len*(len + 1)/2 +1;
+    char* explodedString = (char*)malloc(explodedLength);//space is allocated for exploded string 
+    if(explodedString == NULL){
+        return NULL;
+    }
+    int d, s, r = 0;
+    for (d = 0; d < len; d++) {//loop that completes the algorithm for the mysteryexplode
+        for (s = 0; s <= d-1; s++) {
+            explodedString[r++] = str[s];//values are appended to exploded string
+        }
+    }
+    explodedString[r] = '\0';
+    return explodedString;
+}
+    
